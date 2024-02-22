867925103f17ab4a0034dded9a7f0975de5e0763
nability <151288742+nability@users.noreply.github.com>
2024-02-22 23:33:17 +0700

    Belanja.py

--- Nabiel/Belanja.py
+++ Nabiel/Belanja.py
@@ -1,3 +1,6 @@
-totalBelanja = int(input("Total belanja : "))
-Dibayar = int(input("Harga : "))
-Totalbayar = 
+TotalBelanja = int(input("Berapa Total Belanja : "))
+
+if (TotalBelanja >= 60000):
+    hargaAkhir = TotalBelanja - 10000
+    print("Selamat anda mendapatkan diskon 10000")
+    print("totalBayar = Rp. ",round(hargaAkhir))
